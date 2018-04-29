e0042ede0925e430be31acb9ec8a0cfe8e5f725e
Jason Kulatunga <jk17@ualberta.ca>
2018-04-29 16:35:40 -0700

    proxy test shoudl correctly generate pac file.

--- pkg/actions/proxy_test.go
+++ pkg/actions/proxy_test.go
@@ -1,1 +1,55 @@
 package actions_test
+
+import (
+	"path"
+	"github.com/stretchr/testify/require"
+	"drawbridge/pkg/actions"
+	"path/filepath"
+	"testing"
+	"io/ioutil"
+	"os"
+	"drawbridge/pkg/config"
+	"drawbridge/pkg/utils"
+)
+
+func TestProxyAction_Start(t *testing.T) {
+	t.Parallel()
+
+	//setup
+	configData, err := config.Create()
+	require.NoError(t, err)
+
+	parentPath, err := ioutil.TempDir("", "")
+	defer os.RemoveAll(parentPath)
+	defer patchEnv("HOME", parentPath)()
+	drawbridgePath := path.Join(parentPath, "drawbridge")
+	err = utils.CopyDir(path.Join("testdata", "delete"), drawbridgePath)
+	require.NoError(t, err, "should not raise an error when deleting answer file")
+
+	configData.Set("options.config_dir", drawbridgePath)
+	configData.Set("config_templates.default.pem_filepath", "test_rsa.pem")
+	configData.Set("options.pem_dir", drawbridgePath)
+	proxyAction := actions.ProxyAction{
+		Config: configData,
+	}
+
+	//test
+	err = proxyAction.Start([]map[string]interface{}{
+		{
+			"environment": "prod",
+			"stack_name": "app",
+			"shard": "us-east-1",
+			"shard_type": "idle",
+			"username": "aws",
+			"config": map[string]interface{}{
+				"filepath": path.Join(drawbridgePath, "prod-app-idle-us-east-1"),
+			},
+			"config_dir": drawbridgePath,
+		},
+	}, true)
+
+
+	//assert
+	require.NoError(t, err, "should not raise an error when generating pac file")
+	require.FileExists(t, filepath.Join(parentPath, "drawbridge.pac"))
+}
