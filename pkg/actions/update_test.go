72f39bbaccdf9d4cb44cbfd50d1545dc477e837a
Jason Kulatunga <jk17@ualberta.ca>
2018-04-29 10:20:33 -0700

    adding tests.

--- pkg/actions/update_test.go
+++ pkg/actions/update_test.go
@@ -1,1 +1,22 @@
 package actions_test
+
+import (
+	"testing"
+	"github.com/stretchr/testify/require"
+	"drawbridge/pkg/actions"
+)
+
+func TestUpdateAction_GetLatestReleaseInfo(t *testing.T) {
+	t.Parallel()
+
+	//setup
+	updateAction := actions.UpdateAction{}
+
+	//test
+	releaseInfo, err := updateAction.GetLatestReleaseInfo()
+
+
+	//assert
+	require.NoError(t, err, "should not raise an error when retrieving release info")
+	require.Equal(t, 3 , len(releaseInfo.Assets), "should correctly retrieve download info for 3 binaries")
+}
