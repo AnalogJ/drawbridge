6b9cd4a71ea307e98d74f72952fcc30006216666
Jason Kulatunga <jk17@ualberta.ca>
2018-04-29 00:08:33 -0700

    testing write answerfile.

--- pkg/actions/create_test.go
+++ pkg/actions/create_test.go
@@ -1,1 +1,36 @@
 package actions_test
+
+import (
+	"testing"
+	"github.com/stretchr/testify/require"
+	"drawbridge/pkg/actions"
+	"path"
+	"drawbridge/pkg/config"
+	"io/ioutil"
+	"os"
+)
+
+func TestCreateAction_WriteAnswersFile(t *testing.T) {
+	t.Parallel()
+
+	//setup
+	configData, err := config.Create()
+	require.NoError(t, err)
+
+	parentPath, err := ioutil.TempDir("", "")
+	defer os.RemoveAll(parentPath)
+
+	configData.Set("options.config_dir", parentPath)
+	createAction := actions.CreateAction{
+		Config: configData,
+	}
+
+	//test
+	err = createAction.WriteAnswersFile("base_name", map[string]interface{}{
+		"test": "test_data",
+	}, false)
+
+
+	//assert
+	require.NoError(t, err, "should not raise an error when adding writing answer file")
+}
